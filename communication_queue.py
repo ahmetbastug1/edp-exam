06a63b594896f5dece285a12dd500fe917851816
ahmetbastug1 <159809221+ahmetbastug1@users.noreply.github.com>
2025-01-22 11:29:55 +0000

    Add Files communication.py

--- communication_queue.py
+++ communication_queue.py
@@ -4,5 +4,12 @@ def add_to_queue(event):
     communication_queue.append(event)
     
 def process_queue():
-    event = communication_queue.pop(0)
-    print(f"Processing event: {event.name}")
+    if communication_queue:  
+        event = communication_queue.pop(0)  
+        print(f"Processing event: {event.name}")
+        if event.name == "ApplicationSubmittedEvent":
+            print(f"Application submitted for: {event.payload['applicant_name']} applying for {event.payload['position_applied_for']}")
+        elif event.name == "ApplicationRejectedEvent":
+            print(f"Application rejected for: {event.payload['applicant_name']} for the position of {event.payload['position_applied_for']}")
+    else:
+        print("No events in the queue to process.")
